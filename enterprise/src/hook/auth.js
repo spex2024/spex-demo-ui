03278fd11953b047554a4565dbe710670e37a446
spex2024 <dercolbags@gmail.com>
2024-09-15 16:18:37 +0000

    v1.0.0.0

--- enterprise/src/hook/auth.js
+++ enterprise/src/hook/auth.js
@@ -12,7 +12,7 @@ const useAuth = () => {
     const { setIsAuthenticated ,logout: clearAuth} = useAuthStore()
     const {  fetchUser } = useUserStore()
     // const baseurl = 'https://enterprise-backend.vercel.app';
-    const baseurl = 'https://enterprise-backend-l6pn.onrender.com';
+    const baseurl = 'https://api.spexafrica.site';
     // const baseurl = 'http://localhost:8080';
 
     const login = async (data) => {
---
+++ enterprise/src/middleware.js
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+    const token = req.cookies.get('token')?.value; // Use the Next.js cookies API
+    const url = new URL(req.url);
+
+    if (token) {
+        // Redirect to home or dashboard if trying to access login or signup page while logged in
+        if (url.pathname === '/login' || url.pathname === '/sign-up') {
+            return NextResponse.redirect(new URL('/', req.url));
+        }
+    } else {
+        // Redirect to login if no token is present
+        if (url.pathname !== '/login' && url.pathname !== '/sign-up') {
+            return NextResponse.redirect(new URL('/login', req.url));
+        }
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/', '/employees', '/vendors', '/orders', '/sign-up', '/add-vendor', '/login'],
+};
