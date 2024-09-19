316d636912f0cf6c46ebbf7ed63b9add4ca5a28b
spex2024 <dercolbags@gmail.com>
2024-09-19 16:54:18 +0000

    v1.0.0.0

--- vendor/src/app/hook/auth.js
+++ vendor/src/app/hook/auth.js
@@ -9,8 +9,8 @@ const useAuth = () => {
     const [error, setError] = useState(null);
     const { setIsAuthenticated } = useAuthStore()
     const router = useRouter();
-    const baseurl = 'http://localhost:8080';
-    // const baseurl = 'https://api.spexafrica.site';
+    // const baseurl = 'http://localhost:8080';
+    const baseurl = 'https://api.spexafrica.site';
 
     const login = async (data) => {
         setError(null);
