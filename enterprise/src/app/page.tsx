e260ddff2fe340517b7de9b745c836b59664ddd6
spex2024 <dercolbags@gmail.com>
2024-10-13 17:44:24 +0000

    v1.0.0.0

--- enterprise/src/app/page.tsx
+++ enterprise/src/app/page.tsx
@@ -2,11 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import useAuthStore from '@/store/authenticate';
-import { ScaleLoader } from 'react-spinners';
 import Dashboard from '@/components/page/dashboard';
-import NotificationModal from '@/components/page/notification';
-import useVendorStore from '@/store/vendors';
 import Footer from "@/components/page/footer";
 import {useUserStore} from "@/store/profile";
 interface User {
