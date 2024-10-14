1b0a9c335250c009891992272c3bc80528f73435
spex2024 <dercolbags@gmail.com>
2024-10-14 20:18:34 +0000

    v1.0.0.0

--- enterprise/src/app/(auth)/login/page.tsx
+++ enterprise/src/app/(auth)/login/page.tsx
@@ -1,22 +1,137 @@
-import Image from "next/image"
-import SignIn from "@/components/auth/sign-in";
+'use client'
 
+import React, { useEffect } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import Link from 'next/link'
+import toast from "react-hot-toast"
+import useAuth from "@/hook/auth"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Eye, EyeOff } from "lucide-react"
 
-export default function Login() {
+const schema = z.object({
+    email: z.string().email({ message: 'Invalid email address' }).nonempty('Email is required'),
+    password: z.string().min(6, { message: 'Password must be at least 6 characters long' }).nonempty('Password is required'),
+})
+
+type SignInFormInputs = z.infer<typeof schema>
+
+export default function SignIn() {
+    const { register, handleSubmit, formState: { errors } } = useForm<SignInFormInputs>({
+        resolver: zodResolver(schema),
+    })
+
+    const { login, success, error } = useAuth()
+    const [showPassword, setShowPassword] = React.useState(false)
+
+    useEffect(() => {
+        if (success) {
+            toast.success(success)
+        } else if (error) {
+            toast.error(error)
+        }
+    }, [success, error])
+
+    const onSubmit: SubmitHandler<SignInFormInputs> = async (data) => {
+        await login(data)
+    }
+
     return (
-        <div className="w-full lg:grid  lg:grid-cols-3 ">
-            <div className="w-full   lg:col-span-1">
-
-                    <SignIn/>
-            </div>
-            <div className="hidden bg-muted lg:block lg:col-span-2">
-                <Image
-                    src="https://res.cloudinary.com/ddwet1dzj/image/upload/v1720541196/spex_jrkich.jpg"
-                    alt="Image"
-                    width="1920"
-                    height="1080"
-                    className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                />
+        <div className="flex min-h-screen bg-cover bg-center bg-no-repeat relative overflow-hidden">
+            <div
+                className="absolute inset-0 bg-black/60"
+                style={{
+                    backgroundImage: "url('https://res.cloudinary.com/ddwet1dzj/image/upload/v1720541341/hero_tg9gt8.jpg')",
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundBlendMode: 'overlay'
+                }}
+            ></div>
+            <div className="flex flex-col md:flex-row items-center justify-center w-full p-4 z-10">
+                <div className="md:w-1/2 max-w-xl text-center md:text-left mb-8 md:mb-0 md:pr-8 animate-fade-in-up">
+                    <h1 className="text-3xl md:text-5xl font-bold text-white mb-4 hidden md:block">Corporate Lunch in Reusable Packs</h1>
+                    <p className="text-sm md:text-base font-light text-white/90">
+                        SPEX simplifies meal planning for your team, saving you time and reducing costs while minimizing plastic waste. Experience convenient, eco-friendly meal solutions that enhance your corporate dining experience.
+                    </p>
+                </div>
+                <Card className="w-full md:w-1/2 max-w-md bg-white/10 backdrop-blur-md border border-white/20 shadow-lg animate-fade-in">
+                    <CardHeader className="space-y-1">
+                        <div className="flex justify-center mb-4">
+                            <img
+                                alt="spex-africa"
+                                className="h-20 w-auto animate-pulse"
+                                src="https://res.cloudinary.com/ddwet1dzj/image/upload/v1722177650/spex_logo-03_png_dui5ur.png"
+                            />
+                        </div>
+                        <CardTitle className="text-xl font-bold text-center text-white">Sign in</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                            <div className="space-y-2">
+                                <Label htmlFor="email" className="text-sm text-white">Email</Label>
+                                <div className="relative">
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="Enter your email"
+                                        {...register('email')}
+                                        className="bg-white/20 border-white/30 text-white placeholder-white/60 text-sm pl-10 focus:ring-2 focus:ring-[#71bc44] transition-all duration-300 ease-in-out placeholder:text-gray-100"
+                                    />
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white/60 absolute left-3 top-1/2 transform -translate-y-1/2" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                                    </svg>
+                                </div>
+                                {errors.email && <p className="text-xs text-red-300 animate-shake">{errors.email.message}</p>}
+                            </div>
+                            <div className="space-y-2">
+                                <Label htmlFor="password" className="text-sm text-white">Password</Label>
+                                <div className="relative">
+                                    <Input
+                                        id="password"
+                                        type={showPassword ? "text" : "password"}
+                                        placeholder="Enter your password"
+                                        {...register('password')}
+                                        className="bg-white/20 border-white/30 text-white placeholder-white/60 text-sm pl-10 focus:ring-2 focus:ring-[#71bc44] transition-all duration-300 ease-in-out placeholder:text-gray-100"
+                                    />
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white/60 absolute left-3 top-1/2 transform -translate-y-1/2" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                                    </svg>
+                                    <button
+                                        type="button"
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        className="absolute right-3 top-1/2 -translate-y-1/2 focus:outline-none"
+                                    >
+                                        {showPassword ? (
+                                            <EyeOff className="h-5 w-5 text-white/60" />
+                                        ) : (
+                                            <Eye className="h-5 w-5 text-white/60" />
+                                        )}
+                                    </button>
+                                </div>
+                                {errors.password && <p className="text-xs text-red-300 animate-shake">{errors.password.message}</p>}
+                            </div>
+                            <Link href="/password/request" className="text-xs text-[#71bc44] hover:text-[#8cd45f] transition-colors duration-300">
+                                Forgot password?
+                            </Link>
+                            <Button type="submit" className="w-full bg-[#71bc44] hover:bg-[#5fa438] text-white text-sm transition-all duration-300 ease-in-out transform hover:scale-105">
+                                Sign In
+                            </Button>
+                        </form>
+                    </CardContent>
+                    <CardFooter>
+                        <p className="text-xs text-center text-white/80 w-full">
+                            Do not have an account?{' '}
+                            <Link href="/sign-up" className="text-[#71bc44] hover:text-[#8cd45f] font-medium transition-colors duration-300">
+                                Sign up for free
+                            </Link>
+                        </p>
+                    </CardFooter>
+                </Card>
             </div>
         </div>
     )
--- enterprise/src/app/(auth)/sign-up/page.tsx
+++ enterprise/src/app/(auth)/sign-up/page.tsx
@@ -1,26 +1,234 @@
-import Image from "next/image";
-import SignUp from "@/components/auth/sign-up";
+'use client'
 
-export default function Register() {
-    return (
-        <div className="w-full lg:grid  lg:grid-cols-3 ">
+import React, { useEffect, useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import Link from 'next/link'
+import { toast } from 'react-hot-toast'
+import useAuth from "@/hook/auth"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { motion } from "framer-motion"
+import { AtSign, Briefcase, MapPin, Phone, Lock } from 'lucide-react'
 
-            {/* Sign-up form */}
-            <div className=" w-full  lg:col-span-1 ">
+const schema = z.object({
+    company: z.string().nonempty('Company is required'),
+    branch: z.string().nonempty('Branch is required'),
+    location: z.string().nonempty('Location is required'),
+    email: z.string().email('Invalid email address').nonempty('Email is required'),
+    password: z.string().min(8, 'Password must be at least 8 characters long').nonempty('Password is required'),
+    confirmPassword: z.string().min(8, 'Password must be at least 8 characters long').nonempty('Confirm Password is required'),
+    phone: z.string().nonempty('Phone number is required'),
+    profilePhoto: z.any().optional(),
+}).refine(data => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword']
+})
 
-                <SignUp/>
+type FormData = z.infer<typeof schema>
+
+const TypewriterText: React.FC<{ text: string }> = ({ text }) => {
+    const [displayText, setDisplayText] = useState('')
+
+    useEffect(() => {
+        let i = 0
+        const typingInterval = setInterval(() => {
+            if (i < text.length) {
+                setDisplayText((prev) => prev + text.charAt(i))
+                i++
+            } else {
+                clearInterval(typingInterval)
+            }
+        }, 100)
+
+        return () => clearInterval(typingInterval)
+    }, [text])
+
+    return <span>{displayText}</span>
+}
+
+const InputWithIcon: React.FC<{
+    icon: React.ReactNode,
+    label: string,
+    name: string,
+    type?: string,
+    register: any,
+    error?: string
+}> = ({ icon, label, name, type = "text", register, error }) => (
+    <div className="relative">
+        <div className="relative">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none text-white/50">
+                {icon}
             </div>
-
-            {/* Image with text overlay */}
-            <div className="relative lg:flex hidden bg-muted  lg:col-span-2">
-                <Image
-                    src="https://res.cloudinary.com/ddwet1dzj/image/upload/v1720541343/hero-1_raxkds.jpg"
-                    alt="Image"
-                    width="1920"
-                    height="1080"
-                    className="h-full w-full object-cover object-center brightness-[0.5] grayscale"
-                />
-            </div>
+            <Input
+                id={name}
+                type={type}
+                {...register(name)}
+                className="bg-white/10 border-white/20 text-white placeholder-white/50 mb-2  pl-10 pr-4 py-2 w-full rounded-md focus:ring-2 focus:ring-[#71bc44] focus:border-transparent transition duration-200 ease-in-out placeholder:text-white"
+                placeholder={label}
+            />
         </div>
-    );
+        {error && <p className="text-red-300 text-xs mt-1">{error}</p>}
+    </div>
+)
+
+export default function Component() {
+    const { register, reset, handleSubmit, watch, formState: { errors } } = useForm<FormData>({
+        resolver: zodResolver(schema)
+    })
+
+    const { createAgency, success, error } = useAuth()
+
+    useEffect(() => {
+        if (success) {
+            toast.success(success)
+        } else if (error) {
+            toast.error(error)
+        }
+    }, [success, error])
+
+    const onSubmit: SubmitHandler<FormData> = async (data) => {
+        const formData = new FormData()
+        formData.append('company', data.company)
+        formData.append('branch', data.branch)
+        formData.append('location', data.location)
+        formData.append('email', data.email)
+        formData.append('password', data.password)
+        formData.append('confirmPassword', data.confirmPassword)
+        formData.append('phone', data.phone)
+        if (data.profilePhoto && data.profilePhoto[0]) {
+            formData.append('profilePhoto', data.profilePhoto[0])
+        }
+
+        try {
+            await createAgency(formData)
+            reset()
+        } catch (error) {
+            console.error('There was an error uploading the image:', error)
+        }
+    }
+
+    const profilePhoto = watch('profilePhoto')
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-cover bg-center relative px-4 py-8" style={{backgroundImage: "url('https://res.cloudinary.com/ddwet1dzj/image/upload/v1720549684/office-1_delwsn.jpg')"}}>
+            <div className="absolute inset-0 bg-black opacity-60"></div>
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+            >
+                <Card className="w-full max-w-2xl relative z-10 bg-white/10 backdrop-blur-md border-white/20 shadow-xl">
+                    <CardHeader className="text-center">
+                        <motion.div
+                            initial={{ scale: 0.8 }}
+                            animate={{ scale: 1 }}
+                            transition={{ duration: 0.5 }}
+                            className="flex justify-center mb-4"
+                        >
+                            <img
+                                alt="spex-africa"
+                                className="w-auto h-24 animate-pulse"
+                                src="https://res.cloudinary.com/ddwet1dzj/image/upload/v1722177650/spex_logo-03_png_dui5ur.png"
+                            />
+                        </motion.div>
+                        <CardTitle className="text-2xl font-bold text-white mb-4">
+                            <TypewriterText text="Embrace Smart Pack!" />
+                        </CardTitle>
+                        <p className="mt-2 text-sm text-gray-200 mb-5">
+                            Be part of the sustainable food packaging revolution! Together, we can make a difference, one smart pack at a time.
+                        </p>
+                    </CardHeader>
+                    <CardContent>
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                                <InputWithIcon
+                                    icon={<Briefcase className="h-5 w-5" />}
+                                    label="Company"
+                                    name="company"
+                                    register={register}
+                                    error={errors.company?.message}
+
+                                />
+                                <InputWithIcon
+                                    icon={<Briefcase className="h-5 w-5" />}
+                                    label="Branch"
+                                    name="branch"
+                                    register={register}
+                                    error={errors.branch?.message}
+                                />
+                                <InputWithIcon
+                                    icon={<MapPin className="h-5 w-5" />}
+                                    label="Location"
+                                    name="location"
+                                    register={register}
+                                    error={errors.location?.message}
+                                />
+                                <InputWithIcon
+                                    icon={<Phone className="h-5 w-5" />}
+                                    label="Phone"
+                                    name="phone"
+                                    type="tel"
+                                    register={register}
+                                    error={errors.phone?.message}
+                                />
+                            </div>
+                            <InputWithIcon
+                                icon={<AtSign className="h-5 w-5" />}
+                                label="Email"
+                                name="email"
+                                type="email"
+                                register={register}
+                                error={errors.email?.message}
+                            />
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                                <InputWithIcon
+                                    icon={<Lock className="h-5 w-5" />}
+                                    label="Password"
+                                    name="password"
+                                    type="password"
+                                    register={register}
+                                    error={errors.password?.message}
+                                />
+                                <InputWithIcon
+                                    icon={<Lock className="h-5 w-5" />}
+                                    label="Confirm Password"
+                                    name="confirmPassword"
+                                    type="password"
+                                    register={register}
+                                    error={errors.confirmPassword?.message}
+                                />
+                            </div>
+                            <div className="flex items-center space-x-4">
+                                <div className="shrink-0">
+                                    <img
+                                        id='preview_img'
+                                        className="h-12 w-12 object-cover rounded-full border-2 border-white/30"
+                                        src={profilePhoto && profilePhoto.length ? URL.createObjectURL(profilePhoto[0]) : 'https://res.cloudinary.com/ddwet1dzj/image/upload/v1722177650/spex_logo-03_png_dui5ur.png'}
+                                        alt="Profile photo preview"
+                                    />
+                                </div>
+                                <Label htmlFor="profilePhoto" className="cursor-pointer bg-white/20 px-4 py-2 rounded-full text-sm font-medium text-white hover:bg-white/30 transition-colors">
+                                    Upload Photo
+                                    <Input id="profilePhoto" type="file" {...register('profilePhoto')} className="hidden" />
+                                </Label>
+                            </div>
+                            <Button type="submit" className="w-full bg-[#71bc44] hover:bg-[#5da438] text-white font-semibold py-2 px-4 rounded-md transition duration-200 ease-in-out transform hover:scale-105">
+                                Sign Up
+                            </Button>
+                        </form>
+                        <p className="mt-6 text-center text-sm text-gray-200">
+                            Already have an account?{' '}
+                            <Link href="/login" className="font-semibold text-[#71bc44] hover:underline">
+                                Sign in
+                            </Link>
+                        </p>
+                    </CardContent>
+                </Card>
+            </motion.div>
+        </div>
+    )
 }
