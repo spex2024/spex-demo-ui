3921f0d11b156c549fd1f410be7255150467129d
spex2024 <dercolbags@gmail.com>
2024-10-16 04:37:09 +0000

    v1.0.0.0

--- enterprise/src/app/(pages)/orders/page.tsx
+++ enterprise/src/app/(pages)/orders/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import OrderData from "@/components/page/orders";
 import useAuthStore from "@/store/authenticate";
 import {useRouter} from "next/navigation";
-import {ClimbingBoxLoader, ScaleLoader} from "react-spinners";
-import {useUserStore} from "@/store/profile";
+import {ScaleLoader} from "react-spinners";
 interface User {
     packs?: number;
     subscription?: string;
@@ -17,35 +16,7 @@ interface UserStore {
     fetchUser: () => Promise<void>;
 }
 const Orders = () => {
-    const { user, fetchUser } = useUserStore() as UserStore;
-    const router = useRouter();
-    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const loadUser = async () => {
-            await fetchUser();
-
-            // Wait for an additional 5 seconds after fetching user data
-            setTimeout(() => {
-                setLoading(false); // Set loading to false after 5 seconds
-            },1000);
-        };
-
-        loadUser();
-    }, [fetchUser]);
-
-    if (loading) {
-        // Show a loading indicator for 5 seconds
-        return (
-            <div className="flex items-center justify-center h-screen">
-                <ClimbingBoxLoader color="#71bc44" size={20} />
-            </div>
-        );
-    }
-
-    if (user?.isActive === false) {
-        router.push('/');
-    }
     return (
         <div>
             <OrderData/>
--- enterprise/src/app/(pages)/vendors/page.tsx
+++ enterprise/src/app/(pages)/vendors/page.tsx
@@ -1,10 +1,9 @@
 'use client'
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import Vendors from "@/components/page/vendors";
 import useAuthStore from "@/store/authenticate";
 import {useRouter} from "next/navigation";
-import {ClimbingBoxLoader, ScaleLoader} from "react-spinners";
-import {useUserStore} from "@/store/profile";
+import {ScaleLoader} from "react-spinners";
 interface User {
     packs?: number;
     subscription?: string;
@@ -16,35 +15,6 @@ interface UserStore {
     fetchUser: () => Promise<void>;
 }
 const Vendor = () => {
-    const { user, fetchUser } = useUserStore() as UserStore;
-    const router = useRouter();
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const loadUser = async () => {
-            await fetchUser();
-
-            // Wait for an additional 5 seconds after fetching user data
-            setTimeout(() => {
-                setLoading(false); // Set loading to false after 5 seconds
-            },1000);
-        };
-
-        loadUser();
-    }, [fetchUser]);
-
-    if (loading) {
-        // Show a loading indicator for 5 seconds
-        return (
-            <div className="flex items-center justify-center h-screen">
-                <ClimbingBoxLoader color="#71bc44" size={20} />
-            </div>
-        );
-    }
-
-    if (user?.isActive === false) {
-        router.push('/');
-    }
     return (
         <div>
             <Vendors/>
