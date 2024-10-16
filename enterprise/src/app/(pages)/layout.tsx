3297564b470cf6ebfff73513571c5af328ab0655
spex2024 <dercolbags@gmail.com>
2024-10-16 03:07:48 +0000

    v1.0.0.0

--- enterprise/src/app/(pages)/layout.tsx
+++ enterprise/src/app/(pages)/layout.tsx
@@ -33,7 +33,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             // Wait for an additional 5 seconds after fetching user data
             setTimeout(() => {
                 setLoading(false); // Set loading to false after 5 seconds
-            }, 5000);
+            }, 2000);
         };
 
         loadUser();
